0b0756c72d17525ce91e29482bef5a18c08398af
EnoneDev <Enone@protonmail.com>
2017-08-05 18:45:53 +0300

    setting welcome and help message

--- template/index.js
+++ template/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-	welcomeMessage: `Bonjour, lis les actualités du foot en envoyant tout simplement "foot" !`,
-	helpMessage: `Mon utilisation est plutôt simple, envois "foot" pour lire les actualités. Je ne suis pas Jarvis mais d'autres fonctionalités viendrons!`
+	welcomeMessage: `Bonjour, lis les actualités du foot en envoyant tout simplement "foot" ou vas dans le menu`,
+	helpMessage: `Mon utilisation est plutôt simple, envois "foot" pour lire les actualités, tu peux aussi aller dans le menu. Je ne suis pas Jarvis mais d'autres fonctionalités viendrons!`
 }
