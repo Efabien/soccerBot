f71f417f8a57af94b70b50b2c391cc33280d1e52
EnoneDev <Enone@protonmail.com>
2017-08-14 22:54:15 +0300

    same

--- setting/menu.js
+++ setting/menu.js
@@ -51,7 +51,11 @@ module.exports = (req, res) => {
 		method: 'POST',
 		json: req.body.setting
 	}).then(response => {
-		console.log(response);
+    if (response.body.error) {
+      throw new Error(response.body.error);
+    } else {
+      console.log(response);
+    }
 		res.send(response)
 	}).catch(error => {
 		console.log(error);
